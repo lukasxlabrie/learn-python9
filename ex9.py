1bc5e521ed8bfcc64bc933a77c1491639b6d2c09
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-07 15:58:28 -0400

    added notes

--- ex9.py
+++ ex9.py
@@ -1,14 +1,16 @@
 # Heres some new strange stuff, remember type it exactly.
 
+#added \n prints a new line
 days = "\nMon\nTue\nWed\nThu\nFri\nSat\nSun"
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+#using the , allows for a var to print, similar to () and {}
 print("Here are the days: ", days)
 print("Here are the months: ", months)
-
-print(""
+ #unsure what this indicates, but according to a Google search the gorup of 3 " allows for text to go on multiple lines and can function as a docstring
+print("""
 Theres sometheing going on here.
 With the three double quotes.
 We'll be able to type as much as we'd like
 Even 4 lines if we want to, or 5, or 6
-"")
+""") #removing one set of " results in a syntax arror
