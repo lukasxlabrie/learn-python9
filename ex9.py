173122484453972097694face18d66df1cc01bbd
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-07 15:54:19 -0400

    removed 1 "

--- ex9.py
+++ ex9.py
@@ -6,9 +6,9 @@ months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 print("Here are the days: ", days)
 print("Here are the months: ", months)
 
-print("""
+print(""
 Theres sometheing going on here.
 With the three double quotes.
 We'll be able to type as much as we'd like
 Even 4 lines if we want to, or 5, or 6
-""")
+"")
